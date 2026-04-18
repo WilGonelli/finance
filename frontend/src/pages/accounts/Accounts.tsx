e569641b1ef71f5d50db44e24da0c2471aab08aa
WilGonelli <wil_gonelli@hotmail.com>
2026-04-18 20:36:22 -0300

    começado o desenvolvimento da tela de contas

--- frontend/src/pages/accounts/Accounts.tsx
+++ frontend/src/pages/accounts/Accounts.tsx
@@ -1,10 +1,18 @@
 import styles from './accounts.module.css'
 import Navbar from '../../components/navbar/Navbar'
 
+import { FaPlus } from "react-icons/fa";
+
 export default function Accounts() {
     return (
         <div className={styles.container}>
             <Navbar pageActive='account' />
+            <div className={styles.body}>
+                <h1 className={styles.title}>Contas</h1>
+                <div className={styles.btnContainer}>
+                    <button><FaPlus className={styles.icon} /> Adicionar conta</button>
+                </div>
+            </div>
         </div>
     )
 }
